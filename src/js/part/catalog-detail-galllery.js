eb84805d0215dd0e85cee505516a7bbc3769b280
Sergey Solovyev <html@laserwar.com>
2021-09-07 15:46:04 +0300

    Some changes. Catalog detail page

---
+++ src/js/part/catalog-detail-galllery.js
@@ -0,0 +1,40 @@
+(function () {
+
+    var galleryTop = new Swiper('.swiper-container--gallery', {
+        spaceBetween: 30,
+        loop: true,
+        loopedSlides: 4,
+        slidesPerView: 1,
+    });
+
+    var galleryThumbs = new Swiper('.swiper-container--gallery-nav', {
+        spaceBetween: 10,
+        direction: 'vertical',
+        centeredSlides: true,
+        touchRatio: 0.2,
+        slideToClickedSlide: true,
+        loop: true,
+        loopedSlides: 4,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            0: {
+                direction: 'horizontal',
+                slidesPerView: 3,
+            },
+            500: {
+                slidesPerView: 3,
+            },
+            768: {
+                direction: 'vertical',
+                slidesPerView: 3,
+            }
+        }
+    });
+
+    galleryTop.controller.control = galleryThumbs;
+    galleryThumbs.controller.control = galleryTop;
+
+})();
