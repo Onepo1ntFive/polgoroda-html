304ae676e740d64b3e4003479a6de210543745c2
Sergey Solovyev <html@laserwar.com>
2021-09-09 17:49:27 +0300

    Update 2021-09-09T14:49:21.232Z

--- js/main.js
+++ js/main.js
@@ -9903,6 +9903,41 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
 "use strict";
 
 (function () {
+  var swiperCarousel = new Swiper('.swiper-container-category', {
+    speed: 600,
+    watchSlidesVisibility: true,
+    slideToClickedSlide: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 20
+      },
+      1200: {
+        slidesPerView: 5,
+        spaceBetween: 30
+      },
+      1440: {
+        slidesPerView: 6,
+        spaceBetween: 30
+      }
+    }
+  });
+})();
+"use strict";
+
+(function () {
   var swiperGallery = new Swiper('.swiper-container-gallery', {
     speed: 600,
     watchOverflow: false,
