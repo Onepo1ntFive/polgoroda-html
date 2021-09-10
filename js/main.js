58072ce8f16688f079f283418ad3b36a4e842a08
Sergey Solovyev <html@laserwar.com>
2021-09-10 10:18:25 +0300

    Update 2021-09-10T07:18:22.259Z

--- js/main.js
+++ js/main.js
@@ -9960,4 +9960,29 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
   });
   swiperGallery.init();
 })();
+"use strict";
+
+(function () {
+  var swiperGallery = new Swiper('.swiper-container-main', {
+    speed: 600,
+    loop: true,
+    effect: 'fade',
+    parallax: true,
+    fadeEffect: {
+      crossFade: true
+    },
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+  });
+})();
 //# sourceMappingURL=maps/main.js.map
